b07a827991c9c9082fa8a227359059488d5b8df6
Dmitry_Malyasov <dmalyasov24@gmail.com>
2022-08-26 15:55:27 +0800

    Task done

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿void Output(int[]? array)
+{
+    for (int i = 0; i < array.Length; ++i)
+        Console.WriteLine(array[i]);
+}
+
+int EvenCount(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; ++i)
+        if (array[i] % 2 == 0)
+            count++;
+    return count;
+}
+
+Random rnd = new Random();
+int[] array = new int[8];
+
+for (int i = 0; i < 8; ++i)
+    array[i] = rnd.Next(100,999);
+
+Output(array);
+Console.WriteLine();
+Console.WriteLine(EvenCount(array));
+
